a2a787df0c44506ca9f56cac048eaa413ba27e08
daguilar04 <diegoaguilar0611@gmail.com>
2018-06-10 12:13:17 -0600

    'Funciones'

--- Referencia.m
+++ Referencia.m
@@ -1,4 +1,16 @@
-function [] = Referencia(imagen)
+function [Ref] = Referencia(imagen)
+
+% Se obtiene el punto de referencia
+
+% Salidas:
+%   Ref      = Referencia
+
+%          Ref =   [ x   y ]
+
+% Entradas:
+%   Imagen       = Imagen filtrada
+
+%Diego Aguilar
 
 stats = regionprops('table',imagen,'Area','Centroid');
 
@@ -6,27 +18,31 @@ stats = regionprops('table',imagen,'Area','Centroid');
 S = table2array(stats);
 Region = [];
 center = [];
+
 % Filtra las regiones con areas muy grandes
 for i=1:length(S(:,1))
     if (S(i,1) < 200) && (S(i,1)>5);
         Region = [S(i,:);Region];
     end
 end
-imshow(imagen)
+%imshow(imagen)
 
+%%Se obtiene punto maximo y minimo
 [MaxValue, RegionYmax] = max(Region(:,3));
 [MinValue, RegionYmin] = min(Region(:,3));
-
 RegionActual = [(Region(RegionYmax,:)); (Region(RegionYmin,:))];
-%RegionActual(2,:) = (Region(RegionYmin,:));
 
-for i=1:length(RegionActual(:,1));
-    center = [RegionActual(i,2),RegionActual(i,3)];
-    viscircles(center, 15);
-end
+%Visualizar puntos maximos y minimos
+%for i=1:length(RegionActual(:,1));
+ %   center = [RegionActual(i,2),RegionActual(i,3)];
+ %   viscircles(center, 15);
+%end
 
+%Calculo de Referencia
 Ref(1,1) = (RegionActual(1,2)+RegionActual(2,2))/2;
 Ref(1,2) = (RegionActual(1,3)-RegionActual(2,3))/2 + RegionActual(2,3);
-viscircles(Ref, 15);
+
+%Visualizar Referencia
+%viscircles(Ref, 15);
 
 end
