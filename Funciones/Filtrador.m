125427414c52494cd4b364be6d14ddc8fcae77a1
Steve Mena Navarro <35941958+steveamena@users.noreply.github.com>
2018-06-12 11:00:32 -0600

    Cometario Filtrador

--- Funciones/Filtrador.m
+++ Funciones/Filtrador.m
@@ -2,13 +2,15 @@ function [imagenFil] = Filtrador(imagen)
 
 % Objetivo: Eliminar segundero por erosionado
 % Salidas:
-%   imagenFil      =  imagen filtrada
+%   imagenFil = imagen filtrada
 % Entradas:
-%   imagen       = Imagen en escala de grises
-%Diego Aguilar
+%   imagen    = imagen en escala de grises
+%Diego Aguilar (C) 2018
 
+%  Define el elemento estructurante
 seSQ = strel('diamond',1);
 
+% Aplica erosiones sucesivas, 3 en total
 imagenFil = imerode(imagen , seSQ);
 imagenFil = imerode(imagenFil , seSQ);
 imagenFil = imerode(imagenFil , seSQ);
